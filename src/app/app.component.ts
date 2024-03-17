9df2f256793b44dddd8972c82cdb8c282ed86218
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2024-03-18 01:25:21 +0200

    feat: used ngx-arakami pacakge to format arabic words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { NgxArkamiArabicConverterService } from 'ngx-arkami-arabic-converter';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'arkami-arabic-converter-app';
+  form: FormGroup;
+  title = 'Arkami Arabic Converter';
+  formattedNumber = '';
+  constructor(
+    private ngxArkamiService: NgxArkamiArabicConverterService
+  ) {
+    this.form = new FormGroup({
+      number: new FormControl(null)
+    });
+
+  }
+
+  convertNumberToArabic(): void {
+    if (!this.form.value.number) {
+      this.formattedNumber = '';
+      return;
+    }
+
+    const isNumber = parseInt(this.form.value.number);
+
+    if (isNumber) {
+      this.formattedNumber = this.ngxArkamiService.convertToArabicWords(this.form.value.number);
+
+    }
+
+    this.formattedNumber;
+  }
 }
